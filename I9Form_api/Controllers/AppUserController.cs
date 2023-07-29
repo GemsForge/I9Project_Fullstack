4a748df1c1b86309b10460be76316abd25d1f85d
browndia <diamond.brown@adp.com>
2023-07-28 22:45:08 -0400

    api(Controller): added Authenticate controller

--- I9Form_api/Controllers/AppUserController.cs
+++ I9Form_api/Controllers/AppUserController.cs
@@ -1,10 +1,20 @@
-﻿using I9Form_domain.AppUser.Entity;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using I9Form_api.Authentication;
+using I9Form_api.Helpers;
+using I9Form_api.Service;
+using I9Form_domain.AppUser.Entity;
+using I9Form_domain.AppUser.Payload.request;
 using I9Form_persistence;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
+// The AppUser controllers will include: GetAll, GetById, Register, Authenticate,
 namespace I9Form_api.Controllers
 {
     [Route("api/[controller]")]
@@ -12,10 +22,17 @@ namespace I9Form_api.Controllers
     public class AppUserController : BaseApiController
     {
         private readonly DataContext _dataContext;
+        private readonly AppUserService _appUserService;
+        private readonly AppSettings _appSettings;
 
-        public AppUserController(DataContext dataContext)
+
+        public AppUserController(DataContext dataContext, IOptions<AppSettings> appSettings, AppUserService appUserService)
         {
-            _dataContext = dataContext;
+            {
+                _dataContext = dataContext;
+                _appSettings = appSettings.Value;
+                _appUserService = appUserService;
+            }
         }
 
 
@@ -26,8 +43,8 @@ namespace I9Form_api.Controllers
             return await _dataContext.Users.ToListAsync();
         }
 
-        // GET api/User/5
-        [HttpGet("{id}")]
+        //Get User by ID: Pass Guid id as parameter
+        [HttpGet("{id}")] //api/appuser/fjofda789fs
         public async Task<ActionResult<User>> GetUser(Guid id)
         {
             return await _dataContext.Users.FindAsync(id);
@@ -39,8 +56,44 @@ namespace I9Form_api.Controllers
         {
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public ActionResult<User> Authenticate(
+        AuthenticateRequest response
+        )
+        {
+            var user = _appUserService.Authenticate(response);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            // return basic user info and authentication token
+            return Ok(new
+            {
+                Id = user.Id,
+                Username = user.Username,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = tokenString
+            });
+        }
+
+            // PUT api/values/5
+            [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
