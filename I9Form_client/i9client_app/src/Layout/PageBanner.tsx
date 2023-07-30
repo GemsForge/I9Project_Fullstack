c7e7e1abb7d3a4b0574b3e6a6b9974af6e97d3af
browndia <diamond.brown@adp.com>
2023-07-30 14:39:21 -0400

    setup(Fold Structure): removed previous files. Beginning from scratch.

--- I9Form_client/i9client_app/src/Layout/PageBanner.tsx
+++
@@ -1,9 +0,0 @@
-export default function PageBanner (){
-    return(
-        <div className="page_banner">
-        <h2>
-          <strong> Welcome to Employee Onboarding </strong>
-        </h2>
-      </div>
-    )
-}
---
+++ I9Form_client/i9client_app/src/app/layout/App.tsx
@@ -0,0 +1,13 @@
+import { Fragment, useEffect, useState } from 'react';
+import './App.css';
+import { Container } from 'semantic-ui-react';
+
+
+function App() {
+ 
+  return (
+   <div><h1> HEY WORLD! </h1></div>
+  );
+}
+
+export default App;
