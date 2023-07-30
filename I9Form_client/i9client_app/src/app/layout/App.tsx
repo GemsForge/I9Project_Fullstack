667ffff299c19ca871643026fb3e0444cd2f4217
browndia <diamond.brown@adp.com>
2023-07-30 15:18:12 -0400

    client(App): Add semantic-i components to list app users.

--- I9Form_client/i9client_app/src/app/layout/App.tsx
+++ I9Form_client/i9client_app/src/app/layout/App.tsx
@@ -1,12 +1,32 @@
-import { Fragment, useEffect, useState } from 'react';
-import './App.css';
-import { Container } from 'semantic-ui-react';
+import {  useEffect,  useState } from 'react';
+import '../layout/styles.css';
+import {  Header, List } from 'semantic-ui-react';
+import axios from 'axios';
 
 
 function App() {
- 
+
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/appuser').then(response => {
+      setUsers(response.data);
+    })
+  })
   return (
-   <div><h1> HEY WORLD! </h1></div>
+    <div >
+      <Header as='h2' icon='user' content='App Users'/>
+      <List>
+        {users.map((user: any) => (
+          <List.Item key={user.id}>
+            
+            {user.firstName} {user.lastName}
+          
+
+          </List.Item>
+        ))}
+
+      </List>
+    </div>
   );
 }
 
--- I9Form_client/i9client_app/src/index.tsx
+++ I9Form_client/i9client_app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './app/layout/App';
+import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
