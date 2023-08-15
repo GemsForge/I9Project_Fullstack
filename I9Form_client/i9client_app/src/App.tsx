45d3ce40780f4b978c5a202db4769b950fcbbbec
browndia <diamond.brown@adp.com>
2023-08-15 16:17:04 -0400

    client(Router): refactor homepage outside of Route... path.

--- I9Form_client/i9client_app/src/App.tsx
+++ I9Form_client/i9client_app/src/App.tsx
@@ -3,24 +3,32 @@ import './app/layout/styles.css';
 import { Container } from 'semantic-ui-react';
 import NavBar from './app/layout/NavBar';
 import { observer } from "mobx-react-lite"
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import HomePage from './app/features/home/HomePage';
 
 
 function App() {
- 
+  const location = useLocation(); //Which path has the user gone too.
+
   //pass the following properties to the appUser dashboard
   return (
-    <div >
-      <NavBar />
-      <Container style={{ marginTop: "7em" }}>
-      {/* Outlet gets swapped with the selected component route */}
-        <Outlet />
-        {/* <LoginView/>  */}
-        {/* <RegisterView/> */}
-        {/* <ApplicantView/> */}
-      </Container>
-
-    </div>
+    <>
+      {/* CHECK path nmae  */}
+      {location.pathname === '/' ? <HomePage /> :
+        (
+          <>
+            <NavBar />
+            <Container style={{ marginTop: "7em" }}>
+              {/* Outlet gets swapped with the selected component route */}
+              <Outlet />
+              {/* <LoginView/>  */}
+              {/* <RegisterView/> */}
+              {/* <ApplicantView/> */}
+            </Container>
+          </>
+        )
+      }
+    </>
   );
 }
 //This will observe all of the observer in the component
--- I9Form_client/i9client_app/src/app/features/home/HomePage.tsx
+++ I9Form_client/i9client_app/src/app/features/home/HomePage.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
 export default function HomePage(){
     return (
     <Container style={{marginTop: '7em'}}>
         <h1> Home Page</h1>
+        <h2>
+            Go to
+             <Link to='/appUsers' > App User Dashboard</Link>
+        </h2>
         
     </Container>
     )
