784d216952f9bacd577e5334c72849b344ff7ed5
browndia <diamond.brown@adp.com>
2023-08-23 12:51:56 -0400

    client(Layout): restyled appUser list into seperate segments.

--- I9Form_client/i9client_app/src/app/features/user/dashboard/appUser.getAll.tsx
+++ I9Form_client/i9client_app/src/app/features/user/dashboard/appUser.getAll.tsx
@@ -2,18 +2,19 @@ import { Segment, Item } from "semantic-ui-react";
 import { useStore } from "../../../stores/store";
 import { observer } from "mobx-react-lite";
 import AppUSerListItem from "./appUser.listItem";
+import { Fragment } from "react";
 
 export default observer(function UserList() {
     const { appUserStore } = useStore();
     const { userByName } = appUserStore
 
     return (
-        <Segment>
-            <Item.Group divided>
+        <>
+            <Fragment>
                 {userByName.map(user => (
                     <AppUSerListItem key={user.id} user={user} />
                 ))}
-            </Item.Group>
-        </Segment>
+            </Fragment>
+        </>
     )
 })
--- I9Form_client/i9client_app/src/app/features/user/dashboard/appUser.listItem.tsx
+++ I9Form_client/i9client_app/src/app/features/user/dashboard/appUser.listItem.tsx
@@ -1,4 +1,4 @@
-import { Button, Item } from "semantic-ui-react";
+import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import User from "../user.type";
 import { Link } from "react-router-dom";
 import { useState, SyntheticEvent } from "react";
@@ -19,25 +19,39 @@ export default function AppUSerListItem({ user }: Props) {
     }
 
     return (
-        <Item key={user.id}>
-            <Item.Content>
-                <Item.Header as='u'>{user.firstName} {user.lastName}</Item.Header>
-                <Item.Meta>{user.id}</Item.Meta>
-                <Item.Description>
-                    <div>{user.email}</div>
-                    <div>{user.username}</div>
-                </Item.Description>
-                <Item.Extra>
-                    {/* Replace onClick function with Link to User details route (onClick={()=> openForm(user_state.id)}) */}
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size="tiny" circular src="/assets/images/avatar.svg" />
+                        <Item.Content>
+
+                            <Item.Header as={Link} to={`/users/${user.id}`}>{user.firstName} {user.lastName}</Item.Header>
+                            <Item.Meta>{user.id}</Item.Meta>
+
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+
+            </Segment>
+            <Segment clearing>
+                <span>
+                    <Icon name='mail' /> {user.email}
+                    <br />
+                    <Icon name='user' /> {user.username}
+                </span>
+                
                     <Button as={Link} to={`/appUsers/${user.id}`} floated='right'
                         content='View' primary />
+                
                     <Button
                         loading={loading_state && target === user.id}
                         onClick={(e) => handleUserDelete(e, user.id)} floated='right'
                         content='Delete'
                         secondary />
-                </Item.Extra>
-            </Item.Content>
-        </Item>
+              
+            </Segment>
+
+        </Segment.Group>
     )
 }
